f4861993c490672092b52b119c1aaf6cd11da78a
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2026-05-27 01:41:59 +0530

    Minor changes.

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,26 @@ import * as THREE from "three";
 import CameraDebug from "@/components/CameraDebug";
 import Cave from "@/components/Cave";
 import CameraRig from "@/components/CameraRig";
+import AnimatedPropsSquash from "@/components/AnimatedPropsSquash";
 
 export default function HomePage() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas
+      
         shadows
         camera={{ position: [0, 0.15, 0.5], fov: 60, near: 0.001, far: 100 }}
         gl={{
           antialias: true,
-          toneMapping: THREE.ACESFilmicToneMapping,
-          toneMappingExposure: 1.2,
+          toneMapping: THREE.CineonToneMapping,
+          toneMappingExposure: 1,
         }}
       >
         <Cave />
         {/* <OrbitControls target={[0, 0.1, 0]} /> */}
         <CameraRig />
-        <CameraDebug />
+        <AnimatedPropsSquash />
+        {/* <CameraDebug /> */}
       </Canvas>
     </div>
   );
